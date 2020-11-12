169a36eeed3b26dc9701462c6c3ef020a8c246af
Lingtax <m.ling@deakin.edu.au>
2020-11-12 18:25:42 +1100

    Roughed out for multiple predictors, only 3 are wired into backend. New figures added,UI structure, and reset button added

--- server.R
+++ server.R
@@ -3,14 +3,19 @@ library(ggplot2)
 library(tidyverse)
 library(lubridate)
 library(shinyjs)
+library(here)
+library(patchwork)
 
 # function to calculate the slope parameter values for the graph 
 calc_weights <- function(inputs, weights) {
   colSums(inputs * weights) * c(1, 1/10, 1/1000, 1/10, 1/1000, 1/100000)
 }
 
+# Core server functionality -----------------------------------------------
+
 server <- function(input, output) {
-  
+
+  # Reset button controls
   observeEvent(input$reset, {
     reset("sidebar")
   })
@@ -21,7 +26,7 @@ server <- function(input, output) {
     })
   
   # contains  pre-modelled parameter weights
-  nv_weights <-  tibble::tribble(
+  nv_depr <-  tibble::tribble(
         ~i,   ~s1,    ~q1,    ~s2,    ~q2,    ~c2,
      4.446, 2.027, -2.257,   0.26,   0.02, -0.136,
      0.956, 0.475, -0.532,   0.37, -0.568,  0.256,
@@ -29,7 +34,7 @@ server <- function(input, output) {
      0.908, 0.241, -0.224, -0.269,  0.654, -0.362
      )
   
-  v_weights <- tibble::tribble(
+  v_depr <- tibble::tribble(
                     ~i,   ~s1,    ~q1,    ~s2,    ~q2,    ~c2,
                  4.494,  2.43, -2.844,  1.963, -2.435,  0.818,
                  0.915, 0.287,  -0.31, -0.372,  0.852, -0.463,
@@ -39,67 +44,88 @@ server <- function(input, output) {
 
 
   
-    # calculates weights given input parameters
+# calculates weights given input parameters------
   
      
-  nv <-  reactive({
-        calc_weights(inputs(), nv_weights)
+  depr_nv <-  reactive({
+        calc_weights(inputs(), nv_depr)
     })
   
-  v <-  reactive({
-        calc_weights(inputs(), v_weights)
+  depr_v <-  reactive({
+        calc_weights(inputs(), v_depr)
     })
   
   
-  # Generates outputs for display
-  output$inputs <- renderTable(t(inputs()))
+# Generates dignostic outputs for display------
+  #output$inputs <- renderTable(t(inputs()))
   #output$v_params <- renderTable(t(v()))
   #output$nv_params <- renderTable(t(nv()))
 
+
+# Reference plot of daily cases -------------------------------------------
+
+  refdat <- readRDS(here::here("data", "refdata.RDS"))
+  refplot <- refdat %>% ggplot(aes(date, positives, colour = state_abbrev)) +
+    geom_line() +
+    theme_classic() +
+    theme(line = element_line(size = 1.5),
+          text = element_text(size = 16),
+          panel.grid.major.y = element_line(colour = "grey80", size = .3)) + 
+    labs(Title = "Daily COVID-19 positive tests",  y = "Positive tests", x = "Date", colour = "State")
   
-  # x =  time  0 == April 6th
+
+
+# Generate and composite plots per page -----------------------------------
+
+  # x =  time  0 == April 8th
   output$deprPlot <- renderPlot({
 
-    ggplot(data.frame(x = 0:209), aes(x)) +
+    p1 <-  ggplot(data.frame(x = 0:209), aes(x)) +
       geom_function(fun = function(x) 
-        nv()[1] +
-          nv()[2]*ifelse(x>74, 74, x) +
-          nv()[3]*ifelse(x>74, 74, x)^2 +
-          nv()[4]*ifelse(x<75, 0, x-75) +
-          nv()[5]*ifelse(x<75, 0, x-75)^2 +
-          nv()[6]*ifelse(x<75, 0, x-75)^3,
+        depr_nv()[1] +
+          depr_nv()[2]*ifelse(x>74, 74, x) +
+          depr_nv()[3]*ifelse(x>74, 74, x)^2 +
+          depr_nv()[4]*ifelse(x<75, 0, x-75) +
+          depr_nv()[5]*ifelse(x<75, 0, x-75)^2 +
+          depr_nv()[6]*ifelse(x<75, 0, x-75)^3,
                     aes(color = "Non-Victoria")
       ) +
       geom_function(fun = function(x) 
-        v()[1] +
-          v()[2]*ifelse(x>74, 74, x) +
-          v()[3]*ifelse(x>74, 74, x)^2 +
-          v()[4]*ifelse(x<75, 0, x-75) +
-          v()[5]*ifelse(x<75, 0, x-75)^2 +
-          v()[6]*ifelse(x<75, 0, x-75)^3,
+        depr_v()[1] +
+          depr_v()[2]*ifelse(x>74, 74, x) +
+          depr_v()[3]*ifelse(x>74, 74, x)^2 +
+          depr_v()[4]*ifelse(x<75, 0, x-75) +
+          depr_v()[5]*ifelse(x<75, 0, x-75)^2 +
+          depr_v()[6]*ifelse(x<75, 0, x-75)^3,
                     aes(color = "Victoria")
       ) +
-      #experimental annotation layers Lockdown 7th July to 26th October
-      geom_vline(xintercept = 92, colour = 'red') + 
-      annotate("text", x = 93, y = 12, hjust = 0,
-               label = "Melbourne enters \nsecond lockdown") + 
-      geom_vline(xintercept = 203, colour = 'red') + 
-      annotate("text", x = 202, y = 12, hjust = 1,
-               label = "Melbourne exits \nsecond lockdown") +
-      
-      coord_cartesian(xlim = c(0, 200), ylim = c(0, 18)) +
+      coord_cartesian(xlim = c(0, 209), ylim = c(0, 18)) +
       theme_classic() +
       scale_color_manual(name = "Group",
                          values = c("Non-Victoria" = "Gold",
                                     "Victoria" = "DarkBlue")
       ) +
       scale_x_continuous(breaks = c(0, 50, 100, 150, 200),
-      labels = as.character(lubridate::dmy("6th April, 2020") + 0:209)[(c(0, 50, 100, 150, 200)+1)]
+      labels = as.character(lubridate::dmy("8th April, 2020") + 0:209)[(c(0, 50, 100, 150, 200)+1)]
       ) +
       theme(line = element_line(size = 1.5),
             text = element_text(size = 16),
             panel.grid.major.y = element_line(colour = "grey80", size = .3)) + 
-      labs(y = "Depression", x = "Days since April 6th", color = "Legend")
+      labs(y = "Depression", x = "Date", color = "Legend") +
+      
+      #experimental annotation layers Lockdown 7th July to 26th October
+      geom_vline(xintercept = 90, colour = 'red') + 
+      annotate("text", x = 93, y = 12, hjust = 0,
+               label = "Melbourne enters \nsecond lockdown") + 
+      geom_vline(xintercept = 201, colour = 'red') + 
+      annotate("text", x = 198, y = 12, hjust = 1,
+               label = "Melbourne exits \nsecond lockdown") 
+
+    
+    p1 / refplot
+
 
   })
+  
+
 }
--- ui.R
+++ ui.R
@@ -6,15 +6,16 @@ ui <- navbarPage("CPAS explorer",
     tabPanel("Dashboard",
              # Sidebar  
              fluidRow(
-               column(3, 
-                      div(id = "sidebar",
+                 div(id = "sidebar",
+               column(2, 
+                      
                       # sliderInput("time",
                       #             "Timepoint:",
                       #             min = -1,
                       #             max = +1,
                       #             value = 0),
                       sliderInput("depr",
-                                  "Relative Deprivation:",
+                                  "Financial Deprivation:",
                                   min = -1,
                                   max = +1,
                                   value = 0),
@@ -28,26 +29,131 @@ ui <- navbarPage("CPAS explorer",
                                   min = -1,
                                   max = +1,
                                   value = 0),
+                      sliderInput("homesatisfy",
+                                  "Satisfaction with home:",
+                                  min = -1,
+                                  max = +1,
+                                  value = 0),
+                      sliderInput("introvert",
+                                  "Parent introversion:",
+                                  min = -1,
+                                  max = +1,
+                                  value = 0),
+                      sliderInput("media",
+                                  "Use of news media:",
+                                  min = -1,
+                                  max = +1,
+                                  value = 0),
+                      sliderInput("plonely",
+                                  "Parent Loneliness:",
+                                  min = -1,
+                                  max = +1,
+                                  value = 0)
+                      
+                      ),
+               column(2,
+                      sliderInput("clonely",
+                                  "Child Loneliness:",
+                                  min = -1,
+                                  max = +1,
+                                  value = 0),
+                      sliderInput("alcohol",
+                                  "Parent Alcohol use:",
+                                  min = -1,
+                                  max = +1,
+                                  value = 0),
+                      sliderInput("conflict",
+                                  "Couple verbal conflict:",
+                                  min = -1,
+                                  max = +1,
+                                  value = 0),
+                      sliderInput("childbehv",
+                                  "Child behaviour problems:",
+                                  min = -1,
+                                  max = +1,
+                                  value = 0),
+                      sliderInput("childno",
+                                  "Number of Children:",
+                                  min = -1,
+                                  max = +1,
+                                  value = 0),
+                      radioButtons("lote", "Primary language other than English:",
+                                   c("No" = 0,
+                                     "Yes" = 1)
+                      ),
+                      radioButtons("atsi", "Aboriginal and Torres Strait Islander status:",
+                                   c("No" = 0,
+                                     "Yes" = 1)
+                      )),
+               column(2, 
+                      radioButtons("nopartner", "Single parent:",
+                                   c("No" = 0,
+                                     "Yes" = 1)
+                      ),
+                      radioButtons("educ", "Low education:",
+                                   c("No" = 0,
+                                     "Yes" = 1)
+                      ),
+                      radioButtons("city", "Major city:",
+                                   c("No" = 0,
+                                     "Yes" = 1)
+                      ),
+                      radioButtons("renting", "Renting:",
+                                   c("No" = 0,
+                                     "Yes" = 1)
+                      ),
+                      radioButtons("chroncond", "Parent chronic health condition:",
+                                   c("No" = 0,
+                                     "Yes" = 1)
+                      ),
+                      radioButtons("mentalcond", "Parent mental health diagnosis:",
+                                   c("No" = 0,
+                                     "Yes" = 1)
+                      ),
+                      radioButtons("adhdasd", "Child ADHD or ASD diagnosis:",
+                                   c("No" = 0,
+                                     "Yes" = 1)
+                      ),
+                      radioButtons("child_home", "Child at home while working:",
+                                   c("No" = 0,
+                                     "Yes" = 1)
+                      ),
                       
                       useShinyjs(), # Include shinyjs in the UI
                       actionButton("reset", "Reset variables")
-                      )
-               ),
+               )
+               
+               ), # div end  - all controls must be inside for reset
                
                # Show a plot of the generated distribution
-               column(9,
-                      tabsetPanel( 
-                          tabPanel("Depression",
-                      "Demonstration text", 
-                      tableOutput("inputs"),
-                      #tableOutput("nv_params"),
-                      #tableOutput("v_params"),
-                      plotOutput("deprPlot")
-                          ),
-                      tabPanel("Other outcome")
-               
+               column(6,
+                      
+                      "The current study investigated the association between the developing COVID-19 crisis in Australia and parent/child mental health outcomes in 2,365 families experiencing sustained lockdown in Victoria compared to the rest of Australia, from April to early November 2020.", 
+                      
+                      br(), 
+                      
+                      tabsetPanel(
+                                  
+                                  tabPanel("Depression",
+                                   # Commented tables (for diagnostics)
+                                #tableOutput("inputs"),
+                                #tableOutput("nv_params"),
+                                #tableOutput("v_params"),
+                                    plotOutput("deprPlot")
+                                    
+                           ),
+                           tabPanel("Child anxiety",
+
+                               # plotOutput("refPlot")
+
+                      ),
+                      tabPanel("Child Depression",
+
+                      #          plotOutput("refPlot")
+
                       )
                )
+             )
              )
              ),
     
@@ -56,18 +162,25 @@ ui <- navbarPage("CPAS explorer",
              fluidPage(
                fluidRow(
                  column(12,
+                        h1("About the CPAS study"),
+                        "The COVID-19 Pandemic Adjustment Survey (CPAS) is a longitudinal research study investigating how Australian families have been affected by the COVID-19 pandemic. Australia experienced early success after a nation-wide lockdown in April 2020. However, the state of Victoria went on to experience an outbreak and one of the longest and strictest lockdowns in the world over July-October 2020, while the rest of Australia had low infection rates and easing restrictions. In the current analysis, we investigated the association between the developing COVID-19 crisis and parent/child mental health outcomes in families experiencing sustained lockdown in Victoria compared to the rest of Australia, from April to early November 2020.",
+                        br(),
+                        br(),
                         "This is an experimental dashboard for displaying model outputs from the CPAS project.",
                         br(),
                         br(), 
-                        "Authors: ",
+                        "Authors: Elizabeth Westrupp, Christopher Greenwood, Mathew Ling, Gery Karantzas, Jacqui Macdonald, Emma Sciberras, Antonina Mikocka-Walus, Robert Cummins, Delyse Hutchinson, Glenn. A Melvin, Julian Fernando, Samantha Teague, Amanda Wood, John Toumbourou, Tomer S Berkowitz, Jake Linardon, Lisa Olive, Peter Gregory Enticott, Mark Andrew Stokes, Jane McGillivray, Craig Olsson, Matthew Fuller-Tyszkiewicz and George Youssef",
+                        br(),
+                        br(),
+                        "The CPAS data are augmented by data from ", a(href = "www.covid19data.com.au",  "www.covid19data.com.au"), 
+                        ". Support their work at ", a(href = "https://www.covid19data.com.au/about", "https://www.covid19data.com.au/about"), ".",
+                        br(),
                         br(),
                         "This app was developed in Shiny by", a(href = "http://twitter.com/Lingtax", "Mathew Ling")
                  )
                )
              )
     )
-    
-    
-    
+
 )
 
